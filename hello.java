fe822e2fe740b704a59b70f92049ed290c5223e5
Darren Rabbitt <darrenrabbitt@yahoo.com>
2015-05-03 20:25:54 +0100

    first commit

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class helloJava
 {
 	public static void main(String [] args)
 	{
-	System.out.println("hello there");
+	System.out.println("hello");
 	System.getProperty("java.version");
 	}
 }
--- helloJava.java
+++
@@ -1,8 +0,0 @@
-class helloJava
-{
-	public static void main(String [] args)
-	{
-	System.out.println("hello");
-	System.getProperty("java.version");
-	}
-}
